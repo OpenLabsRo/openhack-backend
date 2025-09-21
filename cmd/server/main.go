481e6015bda06fc6d503fc838ed8ed6584052afd
Soare Catalin <sunshine640@proton.me>
2025-09-21 23:19:04 +0300

    feat: Introduce deployment-specific port configuration
    This commit introduces a mechanism to configure the application's port based on the deployment type (`dev`, `prod`, or `test`).
    
    Previously, the port was configured using the `env.PORT` environment variable. This change removes that dependency and hardcodes the ports for each deployment type:
    
    -   `dev`: 9001
    -   `prod`: 9002
    -   `test`: 9000
    
    This change simplifies the deployment process and makes it more explicit.
    
    To support this change, this commit also introduces three new shell scripts:
    
    -   `run_dev.sh`: Runs the application in development mode.
    -   `run_prod.sh`: Runs the application in production mode.
    -   `run_test.sh`: Runs the application in test mode.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -17,13 +17,26 @@ func main() {
 		os.Exit(1)
 	}
 	deployment := os.Args[1]
+
+	port := ""
+	switch deployment {
+	case "test":
+		port = "9000"
+	case "dev":
+		port = "9001"
+	case "prod":
+		port = "9002"
+	default:
+		log.Fatalf("Invalid deployment type: %s", deployment)
+	}
+
 	app := internal.SetupApp(deployment)
 
 	app.Get("/testflags", models.FlagsMiddlewareBuilder([]string{"test"}))
 
-	if app.Listen(fmt.Sprintf(":%v", env.PORT), fiber.ListenConfig{
+	if app.Listen(fmt.Sprintf(":%s", port), fiber.ListenConfig{
 		EnablePrefork: env.PREFORK,
 	}) != nil {
-		log.Fatalf("Error listening on port %v", env.PORT)
+		log.Fatalf("Error listening on port %s", port)
 	}
 }
